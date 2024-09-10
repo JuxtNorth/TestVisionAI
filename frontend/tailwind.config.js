577b691e2361bf1cca2441b779e5b2dd68886aac
wtrm3ln <142867065+wtrm3ln@users.noreply.github.com>
2024-09-10 22:20:20 +0530

    tailwind, react wouter and landing page setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',               // Include index.html file
+    './src/**/*.{js,ts,jsx,tsx}',  // Include all JS, TS, JSX, and TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
