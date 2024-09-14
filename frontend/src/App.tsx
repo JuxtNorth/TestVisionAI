6fc422cd3f2caa5431653684ba14499a5d96883b
Arpit Agarwal <arpitmanas28@gmail.com>
2024-09-14 14:44:48 +0530

    Added Theme related configs in base setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,13 @@
-import TestVision from "./screens/TestVision";
-import LandingPage from "./screens/LandingPage";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css';
+import TestVision from './screens/TestVision';
+import LandingPage from './screens/LandingPage';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/test-vision" element={<TestVision />} />
-        </Routes>
-    </Router>
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/test-vision" element={<TestVision />} />
+    </Routes>
   );
 }
 
